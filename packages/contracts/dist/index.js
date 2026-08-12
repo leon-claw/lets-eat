8dd1e5d3edba0e1182aa6cbe84675e3d3f44c5b8
weijianghong <weijianghong@tuzhanai.com>
2026-08-13 00:10:58 +0800

    chore: ignore generated contract build

--- packages/contracts/dist/index.js
+++
@@ -1,6 +0,0 @@
-export * from './catalog.js';
-export * from './common.js';
-export * from './identity.js';
-export * from './realtime.js';
-export * from './rooms.js';
-export * from './rounds.js';
